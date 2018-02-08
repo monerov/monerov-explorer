bfb2c249e52e4fb8f0bf51e52bb8ce6184a0f925
instanceme <instanceme@monerov.org>
2018-02-09 01:01:38 +0200

    change config

--- src/templates/js/config.js
+++ src/templates/js/config.js
@@ -3,10 +3,10 @@ var config = {
     coinUnitPlaces: 12,
     txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
     txCoinbaseMinConfirms: 60, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
-    coinSymbol: 'XMR',
-    openAliasPrefix: "xmr",
-    coinName: 'Monero',
-    coinUriPrefix: 'monero:',
+    coinSymbol: 'XMV',
+    openAliasPrefix: "xmv",
+    coinName: 'MoneroV',
+    coinUriPrefix: 'monerov:',
     addressPrefix: 18,
     integratedAddressPrefix: 19,
     subAddressPrefix: 42,
